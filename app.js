5f3fe8130f7ce42ae0857528df66b8c722a7b894
ayush gajera~~ <ayushgajera50@gmail.com>
2024-03-27 11:48:43 +0530

    Page1 Done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline();
+function loadingAnimation(){
+  var tl = gsap.timeline();
 tl.from(".line h1", {
   y: 150,
   stagger: 0.25,
@@ -25,15 +26,42 @@ tl.to(".line h2", {
 });
 tl.to(".loader", {
   opacity: 0,
-  duration: 0.2,
+  duration: 0.8,
   delay: 2.6,
 });
 tl.from(".page1", {
   delay: 0.1,
   y: 1200,
-  duration: 0.5,
+  duration: 1,
   ease: Power4,
 });
 tl.to(".loader", {
   display: "none",
 });
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,h3,#hero4 h1",{
+  y:100,
+  stagger:0.1,
+});
+
+}
+
+loadingAnimation();
+
+
+document.addEventListener("mousemove",function(loc){
+ gsap.to(".crsr",{
+  left:loc.x,
+  top:loc.y,
+  
+ })
+})
+
+
+// Shery.makeMagnet(".nav-part2 h4" /* Element to target.*/, {
+//   //Parameters are optional.
+//   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//   duration: 1,
+// });
+
+
+
